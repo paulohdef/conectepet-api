bdd047a20618ef7ac2c45f6b53aac200719b7fb7
paulohdef <paulohdef1@gmail.com>
2022-06-09 11:08:11 -0300

    deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,16 @@ import { UsersService } from './users.service';
       autoLoadModels: true, //reconhece automaticamente os modelos do projeto (SequelizeModule.forFeature([modelos]))
       synchronize: true,    //quando subir a aplicação, importa automaticamente os modulos(em forFeature) e sincroniza com o banco de dados
     }),
+    // SequelizeModule.forRoot({
+    //   dialect: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'bobowna123',
+    //   database: 'users',
+    //   autoLoadModels: true, //reconhece automaticamente os modelos do projeto (SequelizeModule.forFeature([modelos]))
+    //   synchronize: true,    //quando subir a aplicação, importa automaticamente os modulos(em forFeature) e sincroniza com o banco de dados
+    // }),
     SequelizeModule.forFeature([Users]),
   ],
   controllers: [AppController, UsersController],
