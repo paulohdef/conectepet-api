b35942ed8ecbbc2e23bcf2c6d25b2fae125890e4
paulohdef <paulohdef1@gmail.com>
2022-07-04 18:51:12 -0300

    add return on post of pets

--- src/pets.service.ts
+++ src/pets.service.ts
@@ -5,11 +5,10 @@ import { Pets } from './pets.model';
 
 @Injectable()
 export class PetsService {
-  
   constructor(
     @InjectModel(Pets)
-    private PetModel : typeof Pets
-  ){}
+    private PetModel: typeof Pets,
+  ) {}
 
   async obterTodos(): Promise<Pets[]> {
     return this.PetModel.findAll();
@@ -20,19 +19,20 @@ export class PetsService {
   }
 
   async criar(pet: Pets) {
-    this.PetModel.create(pet)
+    return this.PetModel.create(pet);
+    //this.PetModel.findByPk(id);
   }
 
-  async alterar(pet: Pets): Promise<[number, Pets[]]>{
+  async alterar(pet: Pets): Promise<[number, Pets[]]> {
     return this.PetModel.update(pet, {
-      where : {
-        id : pet.id
-      }
+      where: {
+        id: pet.id,
+      },
     });
   }
 
   async apagar(id: number) {
-    const pet: Pets = await this.obterUm(id)
+    const pet: Pets = await this.obterUm(id);
     pet.destroy();
   }
 }
